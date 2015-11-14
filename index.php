1e8ad5646d64e2e6b4283069d5c1f6fd5988b058
System Administrator <root@Dylans-MacBook-Pro.local>
2015-11-14 10:07:20 -0500

    Carousel added on index page

--- index.php
+++ index.php
@@ -29,6 +29,46 @@
 				<div class="row">
 					<div class="col-lg-10 col-lg-offset-1 bottom-content">
 						<div class="row bottom-content-rows">
+									<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+								  <!-- Indicators -->
+								  <ol class="carousel-indicators">
+								    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+								    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+								    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+								  </ol>
+								 
+								  <!-- Wrapper for slides -->
+								  <div class="carousel-inner">
+								    <div class="item active">
+								      <img src="http://placehold.it/1200x315" alt="...">
+								      <div class="carousel-caption">
+								          <h3>Caption Text</h3>
+								      </div>
+								    </div>
+								    <div class="item">
+								      <img src="http://placehold.it/1200x315" alt="...">
+								      <div class="carousel-caption">
+								          <h3>Caption Text</h3>
+								      </div>
+								    </div>
+								    <div class="item">
+								      <img src="http://placehold.it/1200x315" alt="...">
+								      <div class="carousel-caption">
+								          <h3>Caption Text</h3>
+								      </div>
+								    </div>
+								  </div>
+								 
+								  <!-- Controls -->
+								  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+								    <span class="glyphicon glyphicon-chevron-left"></span>
+								  </a>
+								  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+								    <span class="glyphicon glyphicon-chevron-right"></span>
+								  </a>
+								</div> <!-- Carousel -->
+						</div>
+						<div class="row bottom-content-rows">
 							<div class="col-lg-7 col-xs-12">
 								<h2 class="title">Our vision</h2>
 								<font class="bottom-content-descriptions">
