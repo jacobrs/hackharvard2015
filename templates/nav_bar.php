86fd29651443c26adb9e0cda6d848765f0854f63
benrs <benjaminbarault@gmail.com>
2015-11-14 00:45:06 -0500

    Pushing the nav

---
+++ templates/nav_bar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">AppName</a>
+		</div>
+	    <div class="collapse navbar-collapse">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li class="active"><a href="#">Home</a></li>
+	        <li><a href="#">The Team</a></li>
+	        <li><a href="#">Uploaded Models</a></li>
+	      </ul>
+	    </div>
+	</div>
+</nav>
